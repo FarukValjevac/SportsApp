248c23d28d8266d33edd7844a9983fb4552a919a
Faruk Valjevac <faruk.valjevac@at.bosch.com>
2025-05-25 14:50:00 +0200

    changing PW

--- backend/src/data.source.ts
+++ backend/src/data.source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: '',
+  password: 'root',
   database: 'sportsapp',
   entities: [Booking],
   migrations: [],
